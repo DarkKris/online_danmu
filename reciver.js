e0a70425ac5d43dc8f153fb520ad5688bbbfb457
deantiwang <long4664030@163.com>
2021-04-15 00:06:23 +0800

    fore-end complete

---
+++ reciver.js
@@ -0,0 +1,65 @@
+const DELAY = 15 * 1000;
+
+let danmujs = null;
+let ws = null;
+let container = document.getElementsByClassName("danmu-row");
+let len = container.length;
+
+let count = 0;
+
+window.onload = () => {
+  // registerWs();
+  initDanmu();
+  danmujs.start();
+  test();
+};
+
+function recvMsg(content) {
+  danmujs.sendComment({
+    // moveV: 50,
+    id: Date.now(),
+    duration: DELAY,
+    txt: content,
+    style: {
+      color: "rgb(190, 190, 190)",
+      fontSize: '36px'
+    },
+    noDiscard: true,
+  });
+  return;
+}
+
+function registerWs() {
+  ws = new WebSocket(`ws://deanti.wang/danmu/ws`);
+
+  ws.open = () => {};
+  ws.onmessage = (evt) => {
+    recvMsg(evt.data)
+  }
+}
+
+function initDanmu() {
+  danmujs = new DanmuJs({
+    container: document.getElementById('container'),
+    containerStyle: {
+      zIndex: 100
+    },
+    area: {  //弹幕显示区域
+      start: 0, //区域顶部到播放器顶部所占播放器高度的比例
+      end: 1 //区域底部到播放器顶部所占播放器高度的比例
+    },
+    live: true,
+    bOffset: 20,
+    // channelSize: 100, // 轨道大小
+    mouseControl: false, // 打开鼠标控制, 打开后可监听到 bullet_hover 事件。danmu.on('bullet_hover', function (data) {})
+    mouseControlPause: false
+  });
+}
+
+function test() {
+  const delay = Math.random() * 3 + 0.02;
+  setTimeout(() => {
+    recvMsg("deantiwang: test");
+    test();
+  }, delay * 1000);
+}
