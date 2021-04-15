c8a160261285a251e4f94d400a7f3a16497c6fe9
ConanYu <c.zing@outlook.com>
2021-04-15 23:02:12 +0800

    test: 测试变音字符

--- yali_test/server_test.py
+++ yali_test/server_test.py
@@ -1,16 +1,34 @@
 import requests
 import time
 import json
+from typing import List
 from concurrent.futures import ThreadPoolExecutor
 
 res = []
 
+def append_head_tail(lst_unicodes) -> List[str]:
+    utf8_bytes = bytes()
+    for unicode_char in lst_unicodes:
+        utf8_bytes += unicode_char.encode('utf-8')
+    return utf8_bytes
+
+
 def post_test():
     global res
     url = 'http://danmu.deanti.wang/api'
-    res.append(requests.post(url, data=json.dumps({
-        "content": "hello world",
-    })))
+    string = bytes()
+    for letter in 'hhhhhh':
+        string += letter.encode('utf-8')
+        head_tail = append_head_tail(['\u0326'] * 16)
+        string += head_tail
+    try:
+        res.append(requests.post(url, data=json.dumps({
+            "content": string.decode('utf-8'),
+            "nick": "hhh",
+        }).encode('utf-8')))
+    except Exception as e:
+        print(e)
+
 
 def main():
     global res
